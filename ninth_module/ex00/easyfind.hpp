63aee5b8aa54ec98243b8ff1aaaa0289f33e4255
Ahmed Salem <ahsalem@lab3r5s11.42abudhabi.ae>
2023-01-31 11:51:17 +0400

    Learned iterator

---
+++ ninth_module/ex00/easyfind.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahsalem <ahsalem@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 08:03:24 by ahsalem           #+#    #+#             */
+/*   Updated: 2023/01/31 11:50:41 by ahsalem          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+# include <iostream>
+# include <algorithm>
+template <typename T>
+T easyfind(T a, int b);
+
+//find the first occurance of the second parameter in the first parameter
+#endif
+
+
+T easyfind(T a, int b)
+{
+	T::iterator it = a.begin();
+		it == b
+		
+	++it;
+	rend / rbegi
+	a.end();1
+}
